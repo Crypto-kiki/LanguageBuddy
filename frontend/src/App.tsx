6c936b79707fc2deae04be4a63ca178be6306eb2
Crypto-kiki <ejo2123@naver.com>
2023-12-10 17:56:01 +0900

    sign-in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,14 @@
 import { FC } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/main";
+import SignIn from "./pages/sign-in";
 
 const App: FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/log-in" element={<Main />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<Main />} />
         <Route path="/create" element={<Main />} />
         <Route path="/:postId" element={<Main />} />
